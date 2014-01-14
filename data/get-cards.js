cd609df4007d3051e9e61956aee716eb7e0ed376
szabi22 <szabi22@gmail.com>
2014-01-14 18:27:22 +0000

    - table loading from local HTML

--- data/get-cards.js
+++ data/get-cards.js
@@ -1,11 +1,13 @@
 // Listen for the "show" event being sent from the main add-on code. It means that the panel's about to be shown.
 self.port.on("show", function onShow() {
-	var cardList = $("#table-card-list");
-	$("html body").html(cardList);
-	$('td:nth-child(2),th:nth-child(2)').hide();
-	$('td:nth-child(4),th:nth-child(4)').hide();
-	$('td:nth-child(5),th:nth-child(5)').hide();
-	$('td:nth-child(9),th:nth-child(9)').hide();
 	$('#table-card-list').filterTable();
 	$(".filter-table input").focus();
+	
+	// this is used for fetching the html from the website
+	// var cardList = $("#table-card-list");
+	// $("html body").html(cardList);
+	// $('td:nth-child(2),th:nth-child(2)').hide();
+	// $('td:nth-child(4),th:nth-child(4)').hide();
+	// $('td:nth-child(5),th:nth-child(5)').hide();
+	// $('td:nth-child(9),th:nth-child(9)').hide();
 });
